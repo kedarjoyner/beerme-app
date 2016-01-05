d7c4e7cfc602da2e495608c96e6378f4e5e47475
kedarjoyner <kedarjoyner@gmail.com>
2016-01-05 15:01:22 -0600

    change .emv to .env in server.js

--- server.js
+++ server.js
@@ -6,9 +6,6 @@ app.use(express.static('public'));
 //   res.send('Hello World!');
 // });
 
-var server = app.listen(3000 || process.emv.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+var server = app.listen(3000 || process.env.PORT, function () {
+  
 });
