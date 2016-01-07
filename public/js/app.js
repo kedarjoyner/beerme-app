352b9e8564af5d8b62a5f222e0f03d497610e6e0
kedarjoyner <kedarjoyner@gmail.com>
2016-01-06 21:08:43 -0600

    result styling

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function() {
 	console.log("Is this thing on?");
+	$("#results-container").hide();
 
 	$("#button-search").click(function() {
 		var userInput = $("#read-search").val(); //capture user input
@@ -13,22 +14,46 @@ function getRequest(userInput) {
 
 	var params = {
 		key: "859bf6f8fdcf9f9cd2b69cdf21253a12",
-		name: userInput
+		q: userInput,
+		//name: userInput,
+		withBreweries: "Y",
+		type: "beer"
 	}
 
 	$.ajax({
-		url: 'http://api.brewerydb.com/v2/beers',
+		url: 'http://api.brewerydb.com/v2/search',
 		data: params,
 		dataType: 'json',
 		type: 'GET'
 	}).done(function(data) {
-		console.log(data);
+		//console.log(data);
+		showResults(data);
 	})
 }
 
+function showResults(results) {
+	var peeledResults = results.data;
+	//create variables of items wants to display
+	console.log(results.data);
+	$.each(peeledResults, function(i, item) {
+		var name = item.name;
+		console.log(item.name);
+		$("#results-container").show();
+		$(".results").append("<p class='result-p'>" + name + "</p>");
+	});
+}
 
 
+// // Set the question properties in result
 
+// 	var answererElem = result.find('.profile-link a');
+// 	answererElem.attr( 'href', answerer.user.link );
+// 	answererElem.text( answerer.user.link );
+
+// $.each(result.items, function(i, item) { // looping through items
+// 			var question = showQuestion(item);
+// 			$('.results').append(question);
+// 		});
 
 
 //function showResults(results, function(index, ))
