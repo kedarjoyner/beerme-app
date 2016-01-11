987d6ae1844172173e96e839afe2435dda6d7529
kedarjoyner <kedarjoyner@gmail.com>
2016-01-11 17:32:55 -0600

    display results counter

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	console.log("Is this thing on?");
-	//$(".results-template").hide();
-
 	$("#button-search").click(function() {
 		var userInput = $("#read-search").val(); //capture user input
 		$("#read-search").val(""); // restore user input to placeholder text
@@ -14,7 +12,6 @@ function getRequest(userInput) {
 	var params = {
 		key: "859bf6f8fdcf9f9cd2b69cdf21253a12",
 		q: userInput,
-		//name: userInput,
 		withBreweries: "Y",
 		type: "beer",
 		order: name
@@ -27,14 +24,18 @@ function getRequest(userInput) {
 		type: 'GET'
 	}).done(function(data) {
 		showBeerResults(data);
-		console.log(data);
+
+		//update number of search results spans
+		var numberOfResults = $("dl").length;
+		$("#result-number").text(numberOfResults);
+		$("#search-result").text(userInput);
+		//console.log(data);
 	})
 }
 
 function showBeerResults(results) {
 	// data is defined by api. results turns into what user's input. ".data" allows access into object
 	var peeledResults = results.data;
-	var brewery
 	console.log(results.data);
 
 	// iterate through results and append to page
@@ -45,30 +46,30 @@ function showBeerResults(results) {
 		var abv = item.abv;
 		var category = item.style.category.name;
 		var created = item.createDate;
-		// var breweryName = displayBreweries(item.breweries);
-		// var breweryWebsite = displayBreweries(item.breweries);
 		var description = item.style.description;
+
+		// append items to page using a template
 		var template =	'<dl class="results">' +
 			'<dt>Beer Name:</dt>' +
 				'<dd class="name">'+ name +'</dd>' +
 			'<dt>Abv:<dt>' +
 				'<dd class="abv">'+ abv +'</dd>' +
-			'<dt>Catgory:</dt>' +
+			'<dt>Category:</dt>' +
 				'<dd class="category">'+ category +'</dd>' +
 			'<dt>Date Created:</dt>' +
 				'<dd class="created">' + created +'</dd>' +
 			'<dt>Description:</dt>' +
 				'<dd class="description">' + description +'</dd>' 
 				+ displayBreweries(item.breweries);
-			'</dl>'; 
+			'</dl>';
 
+			//remove hidden cass, append template
 			$(".results-template").removeClass("hidden");
 			$(".results-template .col-lg-12").append(template);
 	});
 }
 
 // how come I don't need (index, item) below?
-
 var displayBreweries = function(breweries) {
 	var breweriesTemplate = "";
 	$.each(breweries, function(index, item) {
@@ -80,35 +81,6 @@ var displayBreweries = function(breweries) {
 	return breweriesTemplate;
 }
 
-
-
-
-// var answererElem = result.find('.profile-link a');
-// 	answererElem.attr( 'href', answerer.user.link );
-// 	answererElem.text( answerer.user.link );
-
-// // Set the question properties in result
-
-// 	var answererElem = result.find('.profile-link a');
-// 	answererElem.attr( 'href', answerer.user.link );
-// 	answererElem.text( answerer.user.link );
-
-// $.each(result.items, function(i, item) { // looping through items
-// 			var question = showQuestion(item);
-// 			$('.results').append(question);
-// 		});
-
-
-// //	.done(function(result){ //this waits for the ajax to return the result. result object is created
-// 		var searchResults = showSearchResults(tag, result.items.length ); // showing 30
-
-// 		$('.search-results').html(searchResults); // make html what searchResults returns
-// 		//$.each is a higher order function. It takes an array and a function as an argument.
-// 		//The function is executed once for each item in the array.
-// 		$.each(result.items, function(i, item) { // looping through items
-// 			var answerer = showAnswerers(item);
-// 			$('.results').append(answerer);
-// 		});
 
 
 
