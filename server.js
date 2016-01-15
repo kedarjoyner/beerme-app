a0ee08bbe07b8c1ffce787d482da6d9ee81abb97
kedarjoyner <kedarjoyner@gmail.com>
2016-01-14 20:10:10 -0600

    separate css and scss

--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
 
-var server = app.listen((process.env.PORT || 3000), function () {
-  
+var BreweryDb = require('brewerydb-node');
+var brewdb = new BreweryDb('859bf6f8fdcf9f9cd2b69cdf21253a12');
+
+app.get('/beers', function (req, res) {
+  brewdb.search.beers({ q: "dogfish" }, function(error, data) {
+  	console.log(error);
+  	console.log(data); // beer
+  	res.json(data) //terminates search
+  });
+});
+
+
+var server = app.listen((process.env.PORT || 3000), function () {  
 });
